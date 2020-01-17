1256388aa106172d972c40f8f07823a7e51c2806
Gene Edwards <eedwards@su.edu>
2020-01-16 22:48:12 -0500

    Added a template.html file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const gs = require('github-scraper');
 const inquirer = require("inquirer");
 const fs = require('fs');
-const convertFactory = require('electron-html-to');
+const electron = require('electron-html-to');
 
 inquirer
     //Prompt user to enter a github username
@@ -26,24 +26,64 @@ inquirer
         //Use github-scraper to return the github data based on the username input
         gs(url, function (err, data) {
 
-    
+
             console.log(data);
 
             //HTML to PDF conversion 
-            var conversion = convertFactory({
-                converterPath: convertFactory.converters.PDF
-              });
+            var conversion = electron({
+                converterPath: electron.converters.PDF
+            });
 
-              conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
+            conversion({
+                html: `
+              <!DOCTYPE html>
+<html>
+
+<head>
+    <title>DevGen PDF</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <style>
+        body{
+            background-color: ${response.favColor};
+        }
+        </style>
+        
+</head>
+
+<body>
+<div class="container">
+<img src="${data.avatar}" alt="DeveloperPic" class="img-thumbnail">
+
+
+
+</div>
+    
+       
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
+              
+              
+              ` }, function (err, result) {
                 if (err) {
-                  return console.error(err);
+                    return console.error(err);
                 }
-               
-                result.stream.pipe(fs.createWriteStream('anywhere.pdf'));
-                //conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-              });
+
+                result.stream.pipe(fs.createWriteStream(`${data.username}Resume.pdf`));
+                conversion.kill(); 
+            });
 
         })
     });;
 
-    
