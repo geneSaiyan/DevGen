881c546dde44322b0ed9133854e2b0058b5864fa
Gene Edwards <eedwards@su.edu>
2020-01-18 17:39:55 -0500

    Updated html and added bootstrap for styling

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 //Installing npm packages
-const gs = require('github-scraper');
+const axios = require('axios');
 const inquirer = require("inquirer");
 const electron = require('electron-html-to');
+var gs = require('github-scraper');
 
 inquirer
     //Prompt user to enter a github username and favorite color
@@ -19,21 +20,25 @@ inquirer
         }
     ])
     .then(function (response) {
+
         //Place the username in a variable for the url search
-        var url = `/${response.username}`;
+        var githubUrl = `https://api.github.com/users/${response.username}`;
 
-        //Use github-scraper to return the github data based on the username input
-        gs(url, function (err, data) {
+        //Using Axios to retrieve the data from github api
+        axios.get(githubUrl)
+            .then(function (data) {
 
-            console.log(data);
+                //Console.log the returned data
+                console.log(data);
 
-            //HTML to PDF conversion 
-            var conversion = electron({
-                converterPath: electron.converters.PDF
-            });
+                //HTML to PDF conversion  using electron-html-to
+                var conversion = electron({
+                    converterPath: electron.converters.PDF
+                });
 
-            conversion({
-                html: `
+                //Html template for the PDF
+                conversion({
+                    html: `
                 <!DOCTYPE html>
                 <html>
                 
@@ -57,36 +62,39 @@ inquirer
                 </head>
                 
                 <body>
-                <div id="mainDiv">
+                
                     <div class="container">
                         <h1 style="margin-top:2%">DevGen: Profile Generator</h1>
                         <hr>
+                        <div id="mainDiv">
                         <div class="row">
                             <div class="col-4">
                                 <div class="card" style="width: 18rem;">
-                                    <img src="${data.avatar}" class="card-img-top"
+                                    <img src="${data.data.avatar_url}" class="card-img-top"
                                         alt="avatar">
                                     <div class="card-body">
                                         <h5 class="card-title" >${response.username}</h5>
-                                        <a href="https://www.github.com${data.url}" target="_blank" style="width:120px" class="btn btn-primary">My Github</a> 
-                                        <a href="https://www.github.com${data.website}" target="_blank" style="width:120px" class="btn btn-primary">My Blog</a>
+                                        <p><a href="https://www.google.com/maps/search/?api=1&${data.data.location}" target="_blank">${data.data.location}</a></p>
+                                        <a href="${data.data.url}" target="_blank" style="width:120px" class="btn btn-primary">My Github</a> 
+                                        <a href="${data.data.blog}" target="_blank" style="width:120px" class="btn btn-primary">My Blog</a>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-8">
                                     <div class="jumbotron ">
-                                            <h1 class="display-4">${data.name}</h1>
-                                            <p class="lead">${data.bio}</p>
+                                            <h1 class="display-4">${data.data.name}</h1>
+                                            <p class="lead">${data.data.bio}</p>
                                             <hr class="my-4">
-                                            <p>Public Repositories: ${data.repos}</p>
-                                            <p>GitHub Stars: ${data.stars}</p>
-                                            <p>Followers: ${data.followers}</p>
-                                            <p>Following: ${data.following}</p>
+                                            <p>Public Repositories: ${data.data.public_repos}</p>
+                                            <p>GitHub Stars: </p>
+                                            <p>Followers: ${data.data.followers}</p>
+                                            <p>Following: ${data.data.following}</p>
                                           </div>
                             </div>
                         </div>
                     </div>
                     </div>
+                  
 
                     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
                         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -103,14 +111,21 @@ inquirer
 
               ` }, function (err, result) {
 
-                if (err) {
-                    return console.error(err);
-                }
+                    if (err) {
+                        return console.error(err);
+                    }
 
-                //Create the PDF that is unique with the developers github username
-                result.stream.pipe(fs.createWriteStream(`${data.username}Resume.pdf`));
-                conversion.kill(); 
-            });
-        })
+                    //Create the PDF that is unique with the developers github username
+                    result.stream.pipe(fs.createWriteStream(`${data.data.login}Resume.pdf`));
+                    conversion.kill();
+                });
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+
+
     });;
 
